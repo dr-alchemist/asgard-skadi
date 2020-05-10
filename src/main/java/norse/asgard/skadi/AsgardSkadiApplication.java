fe7341b5c1ecae3ba2eeec706af19859f63f1e5f
mjolnir-asgard <49009913+mjolnir-asgard@users.noreply.github.com>
2020-05-09 23:30:16 -0500

    project init...

---
+++ src/main/java/norse/asgard/skadi/AsgardSkadiApplication.java
@@ -0,0 +1,13 @@
+package norse.asgard.skadi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsgardSkadiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsgardSkadiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/norse/asgard/skadi/AsgardSkadiApplicationTests.java
@@ -0,0 +1,13 @@
+package norse.asgard.skadi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsgardSkadiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
